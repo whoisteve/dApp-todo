b1baf29ef6797863b2c6b1d1dda7629a6d739fe3
JATQK <51714853+Julius6599@users.noreply.github.com>
2022-01-05 10:29:01 +0100

    Fixes
    Main Fixes and Console log updates

--- dApp-ToDo/src/app/components/component/component.module.ts
+++ dApp-ToDo/src/app/components/component/component.module.ts
@@ -3,14 +3,12 @@ import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ShowcaseToDoComponent } from '../showcase-to-do/showcase-to-do.component';
 import { TaskComponent } from '../task/task.component';
 import { ToDoComponent } from '../to-do/to-do.component';
 
 
 @NgModule({
   declarations: [
-    ShowcaseToDoComponent,
     TaskComponent,
     ToDoComponent
   ],
@@ -21,7 +19,6 @@ import { ToDoComponent } from '../to-do/to-do.component';
   
   ],
   exports: [
-    ShowcaseToDoComponent,
     TaskComponent,
     ToDoComponent
   ]
